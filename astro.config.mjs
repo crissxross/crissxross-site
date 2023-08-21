b54de667b609069a0767be009eb2afe446950b22
Christine Wilks <crissxross@users.noreply.github.com>
2023-08-21 14:28:47 +0100

    Revert "adds redirects old site urls & renames some archive files"
    This reverts commit 6bec8ccdc312adbbbd35c371d47eef394abc0a71.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,26 +9,7 @@ export default defineConfig({
 		mdx(),
 		sitemap(),
 	],
-  redirects: {
-    '/elit/underbelly': '/archive/underbelly',
-    '/elit/fitting-the-pattern': '/archive/fitting-the-pattern',
-    '/elit/tailspin': '/archive/tailspin',
-    '/elit/rememori': '/archive/rememori',
-    '/oot/outoftouch': '/archive/outoftouch',
-    '/remixworx/cx-trail-remix': '/archive/cx-trail-remix',
-    '/remixworx/upsidedown-chandelier': '/archive/upsidedown-chandelier',
-    '/movingpix/intravenus': '/archive/intravenus',
-    '/movingpix/wedrank': '/archive/wedrank',
-    '/movingpix/heights': '/archive/heights',
-    '/movingpix/socialdis-ease': '/archive/socialdis-ease',
-    '/movingpix/beforewebegin': '/archive/beforewebegin',
-    '/projects/stitched-up': '/works/stitched-up',
-    '/projects/writing-new-body-worlds': '/works/voices',
-    '/home/about': '/about',
-    '/projects': '/works',
-    '/elit': '/archive',
-    '/remixworx': '/archive',
-    '/movingpix': '/archive',
-    '/oot': '/archive',
-  }
+  // experimental: {
+  //   assets: true
+  //  }
 });
